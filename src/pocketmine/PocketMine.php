d127e24d0158bc47a3e66ffc4e9332e5a7bc4b48
kotyara <62548070+kotyaralih@users.noreply.github.com>
2022-03-05 17:10:51 +0500

    Update PocketMine.php

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -249,16 +249,7 @@ JIT_WARNING
 		define('pocketmine\VERSION', $version->getFullVersion(true));
 
 		$composerGitHash = InstalledVersions::getReference('pocketmine/pocketmine-mp');
-		if($composerGitHash !== null){
-			$currentGitHash = explode("-", \pocketmine\GIT_COMMIT)[0];
-			if($currentGitHash !== $composerGitHash){
-				critical_error("Composer dependencies and/or autoloader are out of sync.");
-				critical_error("- Current revision is $currentGitHash");
-				critical_error("- Composer dependencies were last synchronized for revision $composerGitHash");
-				critical_error("Out-of-sync Composer dependencies may result in crashes and classes not being found.");
-				critical_error("Please synchronize Composer dependencies before running the server.");
-				exit(1);
-			}
+		
 		}
 
 		$opts = getopt("", ["data:", "plugins:", "no-wizard", "enable-ansi", "disable-ansi"]);
